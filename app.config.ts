c2bbbf9e4c13812734ed967c1b670345c3758d27
solivansprvill-droid <solivansprvill@gmail.com>
2026-05-07 10:05:35 +0000

    Fix EAS project owner: set to cortexbtc to match projectId

--- app.config.ts
+++ app.config.ts
@@ -41,7 +41,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  owner: "mirofish",
+  owner: "cortexbtc",
   version: "1.0.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
